c0870c14edb9899c46c1c9669b5e0d43296f0c53
Jaep Cuperus <jaepcuperus@hotmail.com>
2016-12-14 15:00:56 +0100

    Added tile scripts

---
+++ Assets/Scripts/PlacableTileBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacableTileBehaviour : MonoBehaviour {
+    private bool isSelected = false;
+
+    public void setSelected(bool boolean) {
+        isSelected = boolean;
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isSelected) {
+            gameObject.transform.position = Camera.main.transform.position;
+        }
+	}
+}
---
+++ Assets/Scripts/PlacableTileEventHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+using System;
+
+public class PlacableTileEventHandler : MonoBehaviour, ITrackableEventHandler {
+
+    private TrackableBehaviour mTrackableBehaviour;
+    private PlacableTileBehaviour tileBehaviour;
+
+    void Start() {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        tileBehaviour = GetComponentInChildren<PlacableTileBehaviour>();
+        if (mTrackableBehaviour) {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    /// <summary>
+    /// Implementation of the ITrackableEventHandler function called when the
+    /// tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || 
+            newStatus == TrackableBehaviour.Status.TRACKED || 
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) 
+        {
+            OnTrackingFound();
+        }
+        else {
+            OnTrackingLost();
+        }
+    }
+
+    private void OnTrackingFound() {
+        Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+        Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+        // Enable rendering:
+        foreach (Renderer component in rendererComponents) {
+            component.enabled = true;
+        }
+
+        tileBehaviour.setSelected(true);
+
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+    }
+
+    private void OnTrackingLost() {
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+    }
+}
